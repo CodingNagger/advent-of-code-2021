e4633e52e07fb4df5a89ac5c7676dc9c73f1a0a4
CodingNagger <contact@codingnagger.com>
2021-12-13 20:55:45 +0000

    Advent of Code 2021 - Day 13 blog post + Day 14 setup

---
+++ src/main/java/com/codingnagger/days/Day14.java
@@ -0,0 +1,15 @@
+package com.codingnagger.days;
+
+import java.util.List;
+
+public class Day14 implements Day {
+    @Override
+    public String partOne(List<String> input) {
+        return null;
+    }
+
+    @Override
+    public String partTwo(List<String> input) {
+        return null;
+    }
+}
---
+++ src/test/java/com/codingnagger/days/Day14Test.java
@@ -0,0 +1,27 @@
+package com.codingnagger.days;
+
+import com.codingnagger.utils.InputLoader;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Day14Test {
+    private static final List<String> INPUT = InputLoader.LoadTest("day14.txt");
+    private static final Day DAY = new Day14();
+
+    @Test
+    public void partOne_shoudlYieldCorrectResult() {
+        String result = DAY.partOne(INPUT);
+
+        assertThat(result).isEqualTo(null);
+    }
+
+    @Test
+    public void partTwo_shoudlYieldCorrectResult() {
+        String result = DAY.partTwo(INPUT);
+
+        assertThat(result).isEqualTo(null);
+    }
+}
